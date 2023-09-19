49301e7c4d4140708197fc0338552a4d36275a29
Eric Tatchell <erictatch@gmail.com>
2023-09-18 21:38:37 -0700

    open file

--- Wave3931/Form1.cs
+++ Wave3931/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Wave3931
 {
@@ -67,6 +68,55 @@ namespace Wave3931
         }
 
         private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Txt files for testing hahaha(*.txt)|*.txt|MS-WAVE Files (*.wav)|*.wav|All Files (*.*)|*.*"; // Filter for text files, but you can change it to match your file type
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // User selected a file
+                    string selectedFilePath = openFileDialog.FileName;
+                    toolStripStatusLabel1.Text = "Selected File: " + System.IO.Path.GetFileName(selectedFilePath);
+
+                    // Now you can open and read the file as needed
+                    // For example, you can display the content in a TextBox:
+                    //string fileContent = System.IO.File.ReadAllText(selectedFilePath);
+                    //textBox1.Text = fileContent; // Assuming you have a TextBox named textBox1
+                }
+            }
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel2_Click(object sender, EventArgs e)
+        {
+            // Get the name of the selected file from toolStripStatusLabel1
+            string selectedFileName = toolStripStatusLabel1.Text.Replace("Selected File: ", "");
+
+            // Use the selectedFileName as needed
+            MessageBox.Show("Selected File Name: " + selectedFileName);
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
         }
